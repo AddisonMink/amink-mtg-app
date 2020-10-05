a85509eb61007fe69647ae1876a90a22c9b1295b
Addison Mink <addison.mink@example.com>
2020-10-04 21:55:42 -0500

    implemented player view

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,26 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
+import services.UserService
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+  val controllerComponents: ControllerComponents,
+  service: UserService
+)(implicit ec: ExecutionContext) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def userView(id: Long): Action[AnyContent] = Action.async { _ =>
+    service.getUser(id).map {
+      case Left(message) => NotFound(Json.obj("message" -> message))
+      case Right(user) => Ok(views.html.user(user))
+    }
   }
 }
---
+++ app/routers/HomeRouter.scala
@@ -0,0 +1,13 @@
+package routers
+
+import controllers.HomeController
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+import play.api.routing.sird._
+
+class HomeRouter @Inject()(controller: HomeController) extends SimpleRouter {
+  override def routes: Routes = {
+    case GET(p"/user/$id") if id.forall(_.isDigit) => controller.userView(id.toLong)
+  }
+}
